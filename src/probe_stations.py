c06d1af15a38374f6773396e1ba542d4b46cb89e
Eva <evahillmarie@gmail.com>
2026-08-21 20:15:30 +0100

    edits to script
    Added more station names to the STATIONS list.

--- src/probe_stations.py
+++ src/probe_stations.py
@@ -1,6 +1,25 @@
+#just a script to fetch stations
 import requests
+
 TFL_BASE = "https://api.tfl.gov.uk"
+TIMEOUT = 10
 
 STATIONS = {
-  ""
+  "Waterloo",
+  "Liverpool Street",
+  "Tottenham Court Station",
+  "Bond Street",
+  "Bank",
+  "Oxford Street",
+  "Victoria"
+  "London Bridge"
+  "Vauxhall"
+  "Leicester Square"
 }
+
+#write def
+
+#then write def main
+def main():
+
+#then print everything  
